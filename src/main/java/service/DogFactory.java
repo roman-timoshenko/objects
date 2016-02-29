4c2d113d0b8ef2aaf60d482760f0f8937fda9046
semedy <natalie.konyaeva@gmail.com>
2016-03-01 01:09:15 +0300

    next

--- src/main/java/service/DogFactory.java
+++ src/main/java/service/DogFactory.java
@@ -1,12 +1,16 @@
 package service;
 
+import dao.DogNameDao;
 import dog.Dog;
 import dog.DogSize;
 
+import java.io.IOException;
 import java.util.Random;
 
 public class DogFactory {
     Random random = new Random();
+    NameGenerator nameGenerator = new NameGenerator();
+    DogNameDao dogNameDao = new DogNameDao();
 
     public Dog createDog(String name) {
         int age = random.nextInt(10) + 1;
@@ -21,6 +25,13 @@ public class DogFactory {
 
     public Dog createDog(String name, int age) {
         DogSize size = DogSize.SMALL;
+        return new Dog(name, size, age);
+    }
+
+    public Dog createDog() throws IOException {
+        DogSize size = DogSize.SMALL;
+        int age = random.nextInt(10) + 1;
+        String name = nameGenerator.getName(dogNameDao);
         return new Dog(name, size, age);
     }
 }
