165e57609b8c5e28fc4cd0f90c1e9635bf3b72e6
Natalia.Konyaeva <natalie.konyaeva@gmail.com>
2016-03-03 19:03:52 +0300

    oO

--- src/main/java/dogs/Application.java
+++ src/main/java/dogs/Application.java
@@ -29,24 +29,22 @@ public class Application {
           System.out.println("Something went wrong. Please try again");
       } else {
           size = Integer.parseInt(temp[0]);
-          for (int i=0; i < temp.length-1; i++){
-              data[i] = temp[i+1];
-          }
+          System.arraycopy(temp, 1, data, 0, temp.length - 1);
 
       }
       return dogsService.createDogsArray(size, data);
     }
 
     public void printArray(Dog[] dogs) {
-        for (int i = 0; i < dogs.length; i++) {
-            System.out.println(dogs[i].getName() + "\t" + dogs[i].getDogSize() + "\t" + dogs[i].getAge());
+        for (Dog dog : dogs) {
+            System.out.println(dog.getName() + "\t" + dog.getDogSize() + "\t" + dog.getAge());
         }
         System.out.println(" ");
 
     }
 
     public void run() {
-        try {
+       try {
             System.out.println("Please enter array size and dog names in the following format: 5 Sally Dog");
             Dog[] dogs = getDogs();
             System.out.println("Default array of dogs");
